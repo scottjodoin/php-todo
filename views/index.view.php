a53f6fe796c99e6d1fb682b675e5920ab1a4ff64
Scott Jodoin <scottjodoin@gmail.com>
2020-07-14 20:54:55 -0500

    delete todo list from index

--- views/index.view.php
+++ views/index.view.php
@@ -1,14 +1,3 @@
 <?php require('partials/head.php'); ?>
-    <h1>TODO:</h1>
-    <ul>
-      <?php foreach($tasks as $task): ?>
-        <li>
-          <?php if ($task->completed)
-            echo "<strike>" ?>
-            <?= $task->description ?>
-          <?php if ($task->completed)
-            echo "</strike>" ?>
-        </li>
-      <?php endforeach; ?>
-    </ul>
+
 <?php require('partials/footer.php'); ?>
